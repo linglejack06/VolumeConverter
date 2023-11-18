27ff6a64926249d8f86e5554bde23b0dfc92ff7f
Jack Lingle <captainjack0818@gmail.com>
2023-11-18 17:44:46 -0500

    Initial Commit

---
+++ VolumeConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VolumeConverter
+//
+//  Created by Jack Lingle on 11/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VolumeConverter/VolumeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeConverterApp.swift
+//  VolumeConverter
+//
+//  Created by Jack Lingle on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct VolumeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
