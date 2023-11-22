98d6ceaf6d2e25a5676b28cb17a0fe0e3b254dae
Jack Lingle <captainjack0818@gmail.com>
2023-11-22 12:46:06 -0500

    finish output computed type

--- VolumeConverter/ContentView.swift
+++ VolumeConverter/ContentView.swift
@@ -19,11 +19,46 @@ struct ContentView: View {
     @State private var selectedInputType: Volumes = .liters
     @State private var selectedOutputType: Volumes = .milliliters
     @State private var input = 0.0
+    var outputValue: Double {
+        var completeInput: Measurement<UnitVolume>
+        switch(selectedInputType) {
+            case .liters:
+                completeInput = Measurement(value: input, unit: UnitVolume.liters)
+            case .milliliters:
+                completeInput = Measurement(value: input, unit: UnitVolume.milliliters)
+            case .cups:
+                completeInput = Measurement(value: input, unit: UnitVolume.cups)
+            case .pints:
+                completeInput = Measurement(value: input, unit: UnitVolume.pints)
+            case .gallons:
+                completeInput = Measurement(value: input, unit: UnitVolume.gallons)
+        }
+        switch(selectedOutputType) {
+        case .liters:
+            return completeInput.converted(to: UnitVolume.liters).value
+        case .milliliters:
+            return completeInput.converted(to: UnitVolume.milliliters).value
+        case .cups:
+            return completeInput.converted(to: UnitVolume.cups).value
+        case .pints:
+            return completeInput.converted(to: UnitVolume.pints).value
+        case .gallons:
+            return completeInput.converted(to: UnitVolume.gallons).value
+        }
+    }
     var body: some View {
         Form {
             Section("Input Value") {
                 TextField("Value", value: $input, format: .number)
                 Picker("Units", selection: $selectedInputType) {
+                    ForEach(Volumes.allCases) {
+                        Text($0.rawValue)
+                            .tag($0)
+                    }
+                }.pickerStyle(.segmented)
+            }
+            Section("Output: \(outputValue)") {
+                Picker("Output units", selection: $selectedOutputType) {
                     ForEach(Volumes.allCases) {
                         Text($0.rawValue)
                             .tag($0)
